bf581859d8c76072eb52ef8bde193daa7b2e86f8
anurag-rvnkr1 <anurag.rvnkr@gmail.com>
2026-02-03 19:21:05 +0530

    brute-force approach trial
    Straightforward code that checks matching solutions one by one until one is found.

--- TWO-SUM/brute-force-approach.py
+++ TWO-SUM/brute-force-approach.py
@@ -42,4 +42,5 @@ class Solution:
 sol = Solution()
 print(sol.twoSum([2, 7, 11, 15], 9))  
 print(sol.twoSum([3, 2, 4], 6))     
-print(sol.twoSum([3, 3], 6))          
+print(sol.twoSum([3, 3], 6))     
+print(sol.twoSum([2, 5, 1], 6))     
