5539db59c5e82410051b5582fe190d9aa1685daa
anurag-rvnkr1 <anurag.rvnkr@gmail.com>
2026-02-04 13:36:32 +0530

    Optimized Solution for Leetcode
    Store last seen index of each character.
    Instead of moving l one by one → jump directly.

---
+++ Longest Substring Without Repeating Characters/optimized sollution.py
@@ -0,0 +1,44 @@
+"""
+3. Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+Example 2:
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+"""
+# sliding window + hashmap
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_index = {}
+        left = max_len = 0
+        for right, char in enumerate(s):
+            if char in char_index and char_index[char] >= left:
+                left = char_index[char] + 1
+            char_index[char] = right
+            max_len = max(max_len, right - left + 1)
+        return max_len
+
+#Example usage:
+solution = Solution()
+print(solution.lengthOfLongestSubstring("abcabcbb"))  # Output: 3
+print(solution.lengthOfLongestSubstring("bbbbb"))     # Output: 1
+print(solution.lengthOfLongestSubstring("pwwkew"))    # Output: 3
