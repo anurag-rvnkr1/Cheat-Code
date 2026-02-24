0461fc99b693e09a762d373a71d98c8277af07e7
anurag-rvnkr1 <anurag.rvnkr@gmail.com>
2026-02-24 12:05:57 +0530

    Reuse Nodes
    Use dummy node
    
    Compare heads
    
    Attach smaller node
    
    Move pointer
    
    Repeat

---
+++ Merge Two Sorted Lists/Reuse Nodes.py
@@ -0,0 +1,61 @@
+'''
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+    Input: list1 = [1,2,4], list2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
+
+Example 2:
+    Input: list1 = [], list2 = []
+    Output: []
+Example 3:
+    Input: list1 = [], list2 = [0]
+    Output: [0]
+ 
+Constraints:
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+'''
+#Reuse Nodes
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+
+        dummy = ListNode(0)
+        tail = dummy
+
+        while list1 and list2:
+
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+
+        # Attach remaining list
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return dummy.next
+
+#Example Usage
+sol=Solution()
+print(sol.mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, ListNode(4)))))
+print(sol.mergeTwoLists(None, None))
+print(sol.mergeTwoLists(None, ListNode(0)))
