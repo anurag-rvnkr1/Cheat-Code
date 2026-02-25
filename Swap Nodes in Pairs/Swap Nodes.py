561a9d1ccd3ae12d7b09e4a0ddb45b4e0e1e1692
anurag-rvnkr1 <anurag.rvnkr@gmail.com>
2026-02-25 12:15:52 +0530

    Swap Nodes
    Use a dummy node to simplify head swapping.

---
+++ Swap Nodes in Pairs/Swap Nodes.py
@@ -0,0 +1,58 @@
+'''
+24. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example 1:
+    Input: head = [1,2,3,4]
+    Output: [2,1,4,3]
+
+Example 2:
+    Input: head = []
+    Output: []
+
+Example 3:
+    Input: head = [1]
+    Output: [1]
+
+Example 4:
+    Input: head = [1,2,3]
+    Output: [2,1,3]
+
+Constraints:
+    The number of nodes in the list is in the range [0, 100].
+    0 <= Node.val <= 100
+'''
+
+# Swap Nodes
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head):
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+
+        while prev.next and prev.next.next:
+
+            first = prev.next
+            second = first.next
+
+            # Swapping
+            first.next = second.next
+            second.next = first
+            prev.next = second
+
+            # Move prev
+            prev = first
+
+        return dummy.next
+    
+#Example Usage
+sole=Solution()
+print(sole.swapPairs([1,2,3,4]))
