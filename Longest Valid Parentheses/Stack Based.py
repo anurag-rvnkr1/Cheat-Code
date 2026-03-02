a45746004b47a5402d2b872471fdae10e9c956b8
anurag-rvnkr1 <anurag.rvnkr@gmail.com>
2026-03-02 12:59:21 +0530

    Stack Based
    Push -1 initially (base for length calculation)
    
    Traverse string:
    
    If '(' → push index
    
    If ')' → pop stack
    
    After popping:
    
    If stack empty → push current index
    
    Else → calculate length = i - stack[-1]

---
+++ Longest Valid Parentheses/Stack Based.py
@@ -0,0 +1,43 @@
+'''
+32. Longest Valid Parentheses
+
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+    Input: s = "(()"
+    Output: 2
+    Explanation: The longest valid parentheses substring is "()".
+
+Example 2:
+    Input: s = ")()())"
+    Output: 4
+    Explanation: The longest valid parentheses substring is "()()".
+
+Example 3:
+    Input: s = ""
+    Output: 0
+ 
+Constraints:
+    0 <= s.length <= 3 * 104
+    s[i] is '(', or ')'.
+'''
+#Stack Based Solution
+def longestValidParentheses(s):
+    stack = [-1]
+    max_len = 0
+    
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+            
+            if not stack:
+                stack.append(i)
+            else:
+                max_len = max(max_len, i - stack[-1])
+    
+    return max_len
+#Example Usage
+print(longestValidParentheses("(()")) # Output: 2
+print(longestValidParentheses(")()())")) # Output: 4
